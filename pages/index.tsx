ea20f5ba5122cb76657807c84591c7ed0165d800
ja54312 <ija54312@gmail.com>
2022-10-25 12:02:52 -0500

    login estructura base

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,20 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-        <h2>Yard Sale</h2>
+        <div className='login'>
+          <div className='form-container'>
+            <img alt='logo' className='logo'/>
+            <h1 className='title'>Create a new password</h1>
+            <p className='subtitle'>Enter a new password for you account</p>
+            <form className='form'>
+              <label  htmlFor='passwrd' className='label'>Password</label>
+              <input type='password' id='passwrd' placeholder='*********' className='input input-password'/>
+              <label  htmlFor='new-passwrd' className='label'>Password</label>
+              <input type='password' id='new-passwrd' placeholder='*********' className='input input-password'/>
+              <input type='sumbit' value='Confirm' className='primary-button login-button'/>
+            </form>
+          </div>
+        </div>
     </>
   )
 }
