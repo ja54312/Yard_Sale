4618ec84f008b560b266dec0d2600ec9d80ff2e0
ja54312 <ija54312@gmail.com>
2022-10-27 11:36:06 -0500

    ajuste email

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div className='login'>
           <div className='form-container'>
             <img src='./logos/logo_yard_sale.svg' alt='logo' className='logo'/>
-            <h1 className='title'>Create a new password</h1>
+            {/* <h1 className='title'>Create a new password</h1>
             <p className='subtitle'>Enter a new password for you account</p>
             <form className='form'>
               <label  htmlFor='passwrd' className='label'>Password</label>
@@ -14,7 +14,20 @@ const Home = () => {
               <label  htmlFor='new-passwrd' className='label'>Password</label>
               <input type='password' id='new-passwrd' placeholder='*********' className='input input-password'/>
               <input type='sumbit' value='Confirm' className='primary-button login-button'/>
-            </form>
+            </form> */}
+            <h1 className="title">Email has been sent!</h1>
+            <p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
+
+            <div className="email-image">
+              <img src="./icons/email.svg" alt="email"/>
+            </div>
+
+            <button className="primary-button login-button">Login</button>
+
+            <p className="resend">
+              <span>Didn't receive the email?</span>
+              <a href="/">Resend</a>
+            </p>
           </div>
         </div>
     </>
