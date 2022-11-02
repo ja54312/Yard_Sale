ac02c9cce076e9e51a25b96747c8f98942a3c345
ja54312 <ija54312@gmail.com>
2022-11-02 11:15:54 -0600

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,119 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <div className='login'>
+      <section className="main-container">
+        <div className="cards-container">
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+          <div className="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* <div className='login'>
         <div className='form-container'>
           <img src='./logos/logo_yard_sale.svg' alt='logo' className='logo' />
           {/* <h1 className='title'>Create a new password</h1>
@@ -15,7 +127,7 @@ const Home = () => {
               <input type='password' id='new-passwrd' placeholder='*********' className='input input-password'/>
               <input type='sumbit' value='Confirm' className='primary-button login-button'/>
             </form> */}
-          {/* <h1 className="title">Email has been sent!</h1>
+      {/* <h1 className="title">Email has been sent!</h1>
             <p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
 
             <div className="email-image">
@@ -28,7 +140,7 @@ const Home = () => {
               <span>Didn't receive the email?</span>
               <a href="/">Resend</a>
             </p> */}
-          {/* <form action="/" className="form">
+      {/* <form action="/" className="form">
               <label htmlFor="email" className="label">Email address</label>
               <input type="text" id="email" placeholder="platzi@example.cm" className="input input-email"/>
 
@@ -40,7 +152,7 @@ const Home = () => {
             </form>
 
             <button className="secondary-button signup-button">Sign up</button> */}
-          {/* <h1 className="title">My account</h1>
+      {/* <h1 className="title">My account</h1>
             <form action="/"className="form">
               <div className='container-inputsform'>
                 <label htmlFor="name"className="label">Name</label>
@@ -54,19 +166,19 @@ const Home = () => {
               </div>
               <input type="submit" value="Create" className="primary-button login-button"/>
             </form> */}
-          <div>
-            <label htmlFor="name" className="label">Name</label>
-            <p className="value">Camila Yokoo</p>
+      {/* <div>
+        <label htmlFor="name" className="label">Name</label>
+        <p className="value">Camila Yokoo</p>
 
-            <label htmlFor="email" className="label">Email</label>
-            <p className="value">camilayokoo@gmail.com</p>
+        <label htmlFor="email" className="label">Email</label>
+        <p className="value">camilayokoo@gmail.com</p>
 
-            <label htmlFor="password" className="label">Password</label>
-            <p className="value">*********</p>
-            <button className="secondary-button signup-button">Edit</button>
-          </div>
-        </div>
+        <label htmlFor="password" className="label">Password</label>
+        <p className="value">*********</p>
+        <button className="secondary-button signup-button">Edit</button>
       </div>
+    </div>
+      </div > */}
     </>
   )
 }
