4c22382b7b0fa76659185c5e0ecb1a4aabc44ffa
ja54312 <ija54312@gmail.com>
2022-11-09 12:44:08 -0600

    menu desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,19 @@ import React from 'react'
 const Home = () => {
   return (
     <>
+      <div className="desktop-menu">
+        <ul>
+          <li>
+            <a href="/" className="title">My orders</a>
+          </li>
+          <li>
+            <a href="/">My account</a>
+          </li>
+          <li>
+            <a href="/">Sign out</a>
+          </li>
+        </ul>
+      </div>
       <section className="main-container">
         <div className="cards-container">
           <div className="product-card">
