df69f6f9deba5edcd2a75f99641c2c15d0ec27e9
ja54312 <ija54312@gmail.com>
2022-11-11 12:21:09 -0600

    my order

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home = () => {
           </li>
         </ul>
       </div> */}
-      <div className="mobile-menu">
+      {/* <div className="mobile-menu">
         <ul>
           <li>
             <a href="/">CATEGORIES</a>
@@ -60,6 +60,30 @@ const Home = () => {
             </a>
           </li>
         </ul>
+      </div> */}
+      <div className="my-order">
+        <div className="my-order-container">
+          <h1 className="title">My order</h1>
+
+          <div className="my-order-content">
+            <div className="order">
+              <p>
+                <span>03.25.21</span>
+                <span>6 articles</span>
+              </p>
+              <p>$560.00</p>
+            </div>
+
+            <div className="shopping-cart">
+              <figure>
+                <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+              </figure>
+              <p>Bike</p>
+              <p>$30,00</p>
+            </div>
+
+          </div>
+        </div>
       </div>
       <section className="main-container">
         <div className="cards-container">
