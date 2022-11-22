8c5b1f0997684dacd60f5f351c3a07dbead59bd7
ja54312 <ija54312@gmail.com>
2022-11-22 13:09:42 -0600

    shoping cart+

--- pages/index.tsx
+++ pages/index.tsx
@@ -294,6 +294,46 @@ const Home = () => {
           </div>
         </div>
       </section>
+      {/* <aside className="product-detail">
+        <div className="product-detail-close">
+          <img src="./icons/icon_close.png" alt="close" />
+        </div>
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+        <div className="product-info">
+          <p>$35,00</p>
+          <p>Bike</p>
+          <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+          <button className="primary-button add-to-cart-button">
+            <img src="./icons/bt_add_to_cart.svg" alt="add to cart" />
+            Add to cart
+          </button>
+        </div>
+        <div className="dots">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+        </div>
+      </aside> */}
+      <aside className="product-detail">
+        <div className="title-container">
+          <img src="./icons/flechita.svg" alt="arrow" />
+          <p className="title">My order</p>
+        </div>
+        <div className="my-order-content">
+          <div className="shopping-cart">
+            <figure>
+              <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+            </figure>
+            <p>Bike</p>
+            <p>$30,00</p>
+            <img src="./icons/icon_close.png" alt="close" />
+          </div>
+
+          <button className="primary-button">
+            Checkout
+          </button>
+        </div>
+      </aside>
       {/* <div className='login'>
         <div className='form-container'>
           <img src='./logos/logo_yard_sale.svg' alt='logo' className='logo' />
